b2febc4418a05a348a1d3deb32de2fccb43f4042
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-07-15 15:33:25 -0300

    Fixed project chip deformation

--- src/components/Projects/ProjectChip/index.js
+++ src/components/Projects/ProjectChip/index.js
@@ -2,14 +2,8 @@ import React from 'react'
 import styles from './index.module.css'
 import { useHistory } from 'react-router-dom'
 import useProjects from '../../../hooks/useProjects'
-import useTasks from '../../../hooks/useTasks'
 import HelperText from '../../UI/Text/HelperText'
 import HeadingC from '../../UI/Text/HeadingC'
-
-const activeStyles = {
-  transform: 'translateY(-.3rem)',
-  boxShadow: '0 0.188rem 1.25rem 0.125rem rgb(64 62 67 / 16%)',
-}
 
 const ProjectChip = ({ project }) => {
   const { _id, name, colors, date } = project
@@ -32,11 +26,7 @@ const ProjectChip = ({ project }) => {
   }
 
   return (
-    <button
-      style={actualProjectId === _id ? activeStyles : {}}
-      onClick={() => handleClick(_id)}
-      className={styles.projectChipContainer}
-    >
+    <button onClick={() => handleClick(_id)} className={styles.projectChipContainer}>
       <div>
         <span></span>
       </div>
@@ -45,7 +35,9 @@ const ProjectChip = ({ project }) => {
           <div style={colorGradient} className={styles.projectColor}>
             <span className={styles.charName}>{name.charAt(0)}</span>
           </div>
-          <HeadingC>{name}</HeadingC>
+          <div className={styles.projectNameContainer}>
+            <HeadingC>{name}</HeadingC>
+          </div>
         </div>
         <HelperText>Created {parsedDate}</HelperText>
       </div>
