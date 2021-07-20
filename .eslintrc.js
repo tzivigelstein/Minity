a8ad2d2b55bc08844f3b2561988c59621c6ec38e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-07-20 16:31:07 -0300

    Fix missing dependence

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 0,
+    'spaced-comment': 0,
+  },
+}
