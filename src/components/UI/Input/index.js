c0880298b563f3304cfc7cc5e8b23ee336e2c95b
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-20 11:57:31 -0300

    Fix accessibility issues and delete unused vars

--- src/components/UI/Input/index.js
+++ src/components/UI/Input/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './index.module.css'
-import { Times } from '../../Icons'
 
 const Input = ({ Icon, inputProps = { value: '' }, actionButton }) => {
   return (
     <div className={styles.inputWrapper}>
-      {Icon && <Icon className={styles.icon} />}
+      {Icon && (
+        <label style={{ display: 'flex' }} htmlFor={inputProps.id}>
+          <Icon className={styles.icon} />
+        </label>
+      )}
       <div className={styles.inputContainer}>
         <input className={styles.input} type="text" {...inputProps} />
         {actionButton}
