797709c45cd3d1781b57cf395cf9b7368b46c09a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-20 15:05:50 -0300

    Upgrade button accessibility

--- src/components/Tasks/TaskChip/index.js
+++ src/components/Tasks/TaskChip/index.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import styles from './index.module.css'
 import TaskActionsContainer from '../TaskActionContainer'
+import useTasks from '../../../hooks/useTasks'
 
 const TaskChip = ({ task }) => {
+  const { updateTask } = useTasks()
   const { name } = task
 
+  const { state } = task
+
+  const handleChangeTaskState = () => {
+    const newTask = {
+      ...task,
+      state: !state,
+    }
+
+    updateTask(newTask)
+  }
+
   return (
-    <div className={styles.taskChipContainer}>
+    <button onClick={handleChangeTaskState} className={styles.taskChipContainer}>
       <span className={styles.taskName}>{name}</span>
       <TaskActionsContainer task={task} />
-    </div>
+    </button>
   )
 }
 
