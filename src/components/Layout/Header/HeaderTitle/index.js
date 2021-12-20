32c7ab3ebae03687a6227ee83146df5f0139a8b9
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-20 12:17:20 -0300

    Add unsuscribe to clock option

--- src/components/Layout/Header/HeaderTitle/index.js
+++ src/components/Layout/Header/HeaderTitle/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useAuth from '../../../../hooks/useAuth'
 import useClock from '../../../../hooks/useClock'
 import styles from './index.module.css'
 
 const HeaderTitle = () => {
   const { user } = useAuth()
-  const { humanTime } = useClock()
+  const [time, unsuscribe] = useClock()
+
+  useEffect(() => {
+    return () => unsuscribe()
+  }, [])
 
   return (
     <h1 className={styles.headerTitle}>
-      {humanTime}, {user ? user?.name : 'Stranger'}.
+      {time.humanTime}, {user ? user?.name : 'Stranger'}.
     </h1>
   )
 }
