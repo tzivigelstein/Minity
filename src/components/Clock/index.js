1288b78e09180bfcba6ab2069588b279becf9e65
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-20 12:16:20 -0300

    Update component hook implementation

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,10 +2,10 @@ import useTime from '../../hooks/useClock'
 import styles from './index.module.css'
 
 const Clock = () => {
-  const { time } = useTime()
+  const [time] = useTime()
   return (
-    <time dateTime={time} className={styles.clock}>
-      {time}
+    <time dateTime={time.time} className={styles.clock}>
+      {time.time}
     </time>
   )
 }
