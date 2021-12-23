049b2874f38995a0f25b1c0acfd2f312d9bd6cec
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-23 16:21:07 -0300

    Update modal responsibility for its own state

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Times } from '../Icons'
 import styles from './index.module.css'
 
-const Modal = ({ children, title = 'Modal title', description = 'Modal description', setIsOpen }) => {
+const Modal = ({ children, isOpen, title = 'Modal title', description = 'Modal description', setIsOpen }) => {
   const handleCloseModal = () => {
     setIsOpen(false)
   }
@@ -12,18 +12,22 @@ const Modal = ({ children, title = 'Modal title', description = 'Modal descripti
   }
 
   return (
-    <div onClick={handleCloseModal} className={styles.modalWrapper}>
-      <div onClick={handleModalInteraction} className={styles.modalContainer}>
-        <button onClick={handleCloseModal} className={styles.timesButton}>
-          <Times className={styles.timesIcon} width={21} height={21} />
-        </button>
-        <div>
-          <h3 className={styles.modalTitle}>{title}</h3>
-          <p className={styles.modalDescription}>{description}</p>
+    <>
+      {isOpen && (
+        <div onClick={handleCloseModal} className={styles.modalWrapper}>
+          <div onClick={handleModalInteraction} className={styles.modalContainer}>
+            <button onClick={handleCloseModal} className={styles.timesButton}>
+              <Times className={styles.timesIcon} width={21} height={21} />
+            </button>
+            <div>
+              <h3 className={styles.modalTitle}>{title}</h3>
+              <p className={styles.modalDescription}>{description}</p>
+            </div>
+            {children}
+          </div>
         </div>
-        {children}
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
