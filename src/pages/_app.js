6b6807d0c59d64c761a52d6ab83af04b39ee2b4f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-23 16:23:58 -0300

    Delete unused imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,8 @@
-import React from 'react'
 import './index.css'
 import AlertState from '../context/alerts/alertState'
 import AuthState from '../context/auth/authState'
 import ProjectState from '../context/projects/projectState'
 import TaskState from '../context/tasks/tasksState'
-import useAuth from '../hooks/useAuth'
 
 export default function App({ Component, pageProps }) {
   return (
