f85399fb887f3a8a3886dd6fa328e77657bffce4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-03 15:30:13 -0300

    Change prop naming

--- src/components/ActivityIndicator/index.js
+++ src/components/ActivityIndicator/index.js
@@ -1,9 +1,9 @@
 import styles from './index.module.css'
 
 const ActivityIndicator = props => {
-  const { colorStyle } = props
+  const { colorstyle } = props
 
-  const activeClass = colorStyle === 'dark' ? styles.dark : styles.light
+  const activeClass = colorstyle === 'dark' ? styles.dark : styles.light
 
   return (
     <svg className={styles.svg} viewBox="25 25 50 50">
