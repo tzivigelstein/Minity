d02e4b3f25cd5124e9de6a1d63c37168388312e7
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-07 09:53:14 -0300

    Delete unused file

--- src/config/vars.js
+++
@@ -1,8 +0,0 @@
-const enviroment = process.env.NODE_ENV
-
-const productionBackendUrl = process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_URL
-const developmentBackendUrl = process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND_URL
-
-const baseURL = enviroment === 'production' ? productionBackendUrl : developmentBackendUrl
-
-export { baseURL }
