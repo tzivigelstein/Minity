3283c12f3032282ad0a09c3346004cd99fa2d4e8
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-07 09:18:50 -0300

    Update styled link

--- src/components/UI/Links/BaseLink/index.js
+++ src/components/UI/Links/BaseLink/index.js
@@ -1,9 +1,9 @@
 import { forwardRef } from 'react'
 import styles from './index.module.css'
 
-//eslint-disable-next-line
-const BaseLink = forwardRef(({ children, href }, ref) => (
-  <a href={href} ref={ref} className={styles.baseLink}>
+// eslint-disable-next-line react/display-name
+const BaseLink = forwardRef(({ children, href, onClick }, ref) => (
+  <a onClick={onClick} href={href} ref={ref} className={styles.baseLink}>
     {children}
   </a>
 ))
