af46dfabc35cba763755355fe461401ed7944143
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-07 09:18:22 -0300

    Add task list heading loading handling

--- src/components/Tasks/TasksListHeading/index.js
+++ src/components/Tasks/TasksListHeading/index.js
@@ -1,27 +1,32 @@
-import React from 'react'
 import styles from './index.module.css'
 import { Add } from '../../Icons'
 import DynamicButton from '../../UI/Buttons/DynamicButton'
 import useProjects from '../../../hooks/useProjects'
+import useTasks from '../../../hooks/useTasks'
+import ActivityIndicator from '../../ActivityIndicator'
 
 const TasksListHeading = ({ tasks, setIsOpen }) => {
-  const { actualProject } = useProjects()
+  const { currentProject } = useProjects()
+  const { getTasksLoading } = useTasks()
 
-  const { colors = {} } = actualProject || {}
+  const { colors = {} } = currentProject || {}
   const { accentColor = '#1da0f2', secondaryColor = 'lightgreen' } = colors
 
   const backgrounGradient = {
-    background: `linear-gradient(45deg, ${accentColor}, ${secondaryColor})`,
+    background: `linear-gradient(45deg, ${accentColor}, ${secondaryColor})`
   }
 
   const handleAddNewTask = () => {
     setIsOpen(true)
   }
 
+  const numberOfTasks = `${tasks && tasks.length} ${tasks && tasks.length === 1 ? 'task' : 'tasks'}`
+
   return (
     <div className={styles.heading}>
-      <span>
-        {tasks && tasks.length} {tasks && tasks.length === 1 ? 'task' : 'tasks'}
+      <span style={{ display: 'flex', height: '1rem' }}>
+        {getTasksLoading && <ActivityIndicator colorstyle="dark" />}
+        {!getTasksLoading && numberOfTasks}
       </span>
       <DynamicButton onClick={handleAddNewTask} style={backgrounGradient}>
         Add <Add className={styles.addIcon} />
