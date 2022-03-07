422dd11a4eedb0f12b3c4648873ae489a7544127
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-07 10:18:37 -0300

    Add next auth secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,6 +11,7 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     })
   ],
+  secret: process.env.SECRET,
   callbacks: {
     async session({ session, token, user }) {
       session.user.id = user.id
